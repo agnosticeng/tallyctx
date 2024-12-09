f97e3010f9c568fbb8f8b92f8ee270d10783199f
Arnaud Briche <briche.arnaud@gmail.com>
2024-12-09 18:34:19 +0100

    chore: initial import

---
+++ context.go
@@ -0,0 +1,23 @@
+package metrics
+
+import (
+	"context"
+
+	"github.com/uber-go/tally/v4"
+)
+
+type contextKey struct{}
+
+func NewContext(ctx context.Context, scope tally.Scope) context.Context {
+	return context.WithValue(ctx, contextKey{}, scope)
+}
+
+func FromContextOrNoop(ctx context.Context) tally.Scope {
+	v, ok := ctx.Value(contextKey{}).(tally.Scope)
+
+	if !ok {
+		return tally.NoopScope
+	}
+
+	return v
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/agnosticeng/tallyctx
+
+go 1.23.2
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/twmb/murmur3 v1.1.8 // indirect
+	github.com/uber-go/tally/v4 v4.1.16 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
